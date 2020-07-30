c61b17886700600117c7bbf1cb9c51cf3ea82af4
Minho Yun <minhoyun.dev@gmail.com>
2020-07-30 17:56:54 +0900

    Fix

---
+++ src/upload.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-northeast-1",
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "coco-for-dogs",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+export const uploadMiddleware = upload.single("file");
+
+export const uploadController = (req, res) => {
+  const {
+    file: { location },
+  } = req;
+  console.log(location);
+  res.json({ location });
+};

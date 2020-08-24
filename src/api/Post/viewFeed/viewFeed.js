fcff90b82bee7b1c32d24246f195193bb584a10c
Minho Yun <minhoyun.dev@gmail.com>
2020-08-24 13:42:54 +0900

    Fix

--- src/api/Post/viewFeed/viewFeed.js
+++ src/api/Post/viewFeed/viewFeed.js
@@ -2,14 +2,11 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    viewFeed: async (_, args, { request, isAuthenticated }) => {
+    viewFeed: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const { offset, limit } = args;
       const following = await prisma.user({ id: user.id }).following();
       return prisma.posts({
-        first: limit,
-        skip: offset,
         where: {
           user: {
             id_in: [...following.map((user) => user.id), user.id],

4afa23c29958b6b73d8c093eb12ca1254726022a
Minho Yun <minhoyun.dev@gmail.com>
2020-08-31 21:43:48 +0900

    .

--- src/api/User/login/login.js
+++ src/api/User/login/login.js
@@ -5,10 +5,9 @@ export default {
   Mutation: {
     login: async (_, args) => {
       const { email, password } = args;
-      // const user = await prisma.$exists.user({ email });
-      // if (!user) {
-      //   throw Error("登録されていないメールアドレスです。");
-      // }
+      if ((await prisma.$exists.user({ email })) === undefined) {
+        throw Error("登録されていないメールアドレスです。");
+      }
       const { id, password: savedPassword } = await prisma.user({ email });
       if (await decryptPassword(password, savedPassword)) {
         return generateToken(id);

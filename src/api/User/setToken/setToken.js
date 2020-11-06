fcef427ddc83b8af1a3e2519be9ab74664d6102b
Minho Yun <minhoyun.dev@gmail.com>
2020-11-06 14:04:14 +0900

    Add

---
+++ src/api/User/setToken/setToken.js
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    setToken: async (_, args, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { token } = args;
+      const { user } = request;
+
+      return prisma.updateUser({
+        where: { id: user.id },
+        data: {
+          token,
+        },
+      });
+    },
+  },
+};

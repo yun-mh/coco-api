95414358783320fd359df8c98889a9304ff6af99
Minho Yun <minhoyun.dev@gmail.com>
2020-11-07 22:31:04 +0900

    Fix

--- src/api/ChatRoom/ChatRoom.js
+++ src/api/ChatRoom/ChatRoom.js
@@ -2,7 +2,6 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   ChatRoom: {
-    user: ({ id }) => prisma.chatRoom({ id }).user(),
     participants: ({ id }) => prisma.chatRoom({ id }).participants(),
     messages: ({ id }) => prisma.chatRoom({ id }).messages(),
   },

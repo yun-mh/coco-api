ec78bfe72493120f4f682be166ab64359e1ec2e8
Minho Yun <minhoyun.dev@gmail.com>
2020-11-16 11:28:21 +0900

    Update

--- src/api/LostSupport/CreateThread/CreateThread.js
+++ src/api/LostSupport/CreateThread/CreateThread.js
@@ -3,8 +3,8 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createThread: async (_, args) => {
-      console.log(args);
-        // console.log(args);
+      // console.log(args);
+        console.log(args);
       const { dogId, name, breed, age, gender, size, weight, feature, images, lostWhen, lostWhere, phone, email } = args;
       const thread = await prisma.createLostDogThread({
         dog: { connect: { id: dogId }},

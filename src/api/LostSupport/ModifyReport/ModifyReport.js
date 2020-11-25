484aec10878d0bfd39d71c72f35ba8c6620d18fd
Minho Yun <minhoyun.dev@gmail.com>
2020-11-25 20:03:36 +0900

    Fix

--- src/api/LostSupport/ModifyReport/ModifyReport.js
+++ src/api/LostSupport/ModifyReport/ModifyReport.js
@@ -5,8 +5,17 @@ export default {
   Mutation: {
     modifyReport: async (_, args) => {
       // 引数の取得
-      const { reportId, reportType, location, when, name, phone, memo, token } = args;
-      
+      const {
+        reportId,
+        reportType,
+        location,
+        when,
+        name,
+        phone,
+        memo,
+        token,
+      } = args;
+
       // レポートの存在チェック
       const reportExist = await prisma.$exists.lostDogReport({ id: reportId });
       if (reportExist) {
@@ -15,14 +24,14 @@ export default {
           data: { reportType, location, when, name, phone, memo },
           where: { id: reportId },
         });
-        
+
         // トークンの引数が存在したらプッシュ通知を行う
-        if (token !== "" || token !== undefined) {
+        if (token !== "" && token !== undefined) {
           await axios.post("https://exp.host/--/api/v2/push/send", {
             to: token,
             title: "ココ迷子サポート",
             body: "レポートが更新されました。すぐ確認しましょう！",
-          })
+          });
         }
 
         return result;

7f63e7ab3977486c35b6cd3b1e0c9e4d9a6486cd
Minho Yun <minhoyun.dev@gmail.com>
2020-11-27 10:22:23 +0900

    Fix

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const server = new GraphQLServer({
   context: ({ request }) => ({ request, isAuthenticated }),
 });
 
+server.express.use(cors());
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
 server.express.post("/api/upload", cors(), uploadMiddleware, uploadController);

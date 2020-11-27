1f595b44af002e0c02b1b3663931cd4ccc68bc70
Minho Yun <minhoyun.dev@gmail.com>
2020-11-27 10:29:20 +0900

    Fix

--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 // import multerS3 from "multer-s3";
-import * as s3Storage from "multer-sharp-s3";
+import s3Storage from "multer-sharp-s3";
 import aws from "aws-sdk";
 
 const s3 = new aws.S3({

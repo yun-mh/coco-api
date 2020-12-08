31588f64498aeaa17cc94ed766a4a49e33fe046c
Minho Yun <minhoyun.dev@gmail.com>
2020-12-08 17:03:20 +0900

    Add api

---
+++ src/api/Walking/getWalker/getWalker.js
@@ -0,0 +1,10 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    getWalker: async (_, args) => {
+      const { userId } = args;
+      return prisma.walker({ id: userId });
+    },
+  },
+};

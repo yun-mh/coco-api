6edde9a814befabf347409fb4d5f0e9a6864d682
Minho Yun <minhoyun.dev@gmail.com>
2020-12-09 18:14:18 +0900

    update

--- src/api/Walking/getWalker/getWalker.js
+++ src/api/Walking/getWalker/getWalker.js
@@ -3,14 +3,23 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     getWalker: async (_, __, { request, isAuthenticated }) => {
+      // 認証確認
       isAuthenticated(request);
+
+      // ユーザ情報の取得
       const { user } = request;
+
+      // 散歩者情報の検索
       const res = prisma.walkers({
         where: { user: { id: user.id } },
         orderBy: "createdAt_DESC",
       });
-      console.log(res);
-      return res;
+
+      // 散歩者情報が存在すれば一番最新の情報一つを、なければnullをリターンする
+      if (res.length === 0) {
+        return null;
+      }
+      return res[0];
     },
   },
 };

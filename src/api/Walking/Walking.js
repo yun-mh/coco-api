cd53cb259165c20251ea7899a3874582ec52f78c
Minho Yun <minhoyun.dev@gmail.com>
2020-12-11 15:14:23 +0900

    update

--- src/api/Walking/Walking.js
+++ src/api/Walking/Walking.js
@@ -3,9 +3,5 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
   Walker: {
     user: ({ id }) => prisma.walker({ id }).user(),
-    locations: ({ id }) => prisma.walker({ id }).locations(),
-  },
-  Location: {
-    walker: ({ id }) => prisma.location({ id }).walker(),
   },
 };

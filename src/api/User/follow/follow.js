3e926036b3f4cd50d840f75fb7e0d5d52c0d7bb7
Minho Yun <minhoyun.dev@gmail.com>
2020-12-29 16:39:12 +0900

    Update

--- src/api/User/follow/follow.js
+++ src/api/User/follow/follow.js
@@ -1,11 +1,17 @@
+import axios from "axios";
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
     follow: async (_, args, { request, isAuthenticated }) => {
+      // 認証済みの確認
       isAuthenticated(request);
-      const { id: targetId } = args;
+
+      // 各種引数の取得
+      const { id: targetId, token } = args;
       const { user: currentUser } = request;
+
+      // フォロー処理
       try {
         await prisma.updateUser({
           where: { id: currentUser.id },
@@ -17,6 +23,8 @@ export default {
             },
           },
         });
+
+        // 通知のデータを追加
         await prisma.createNotification({
           from: {
             connect: {
@@ -30,6 +38,20 @@ export default {
           },
           type: "FOLLOW",
         });
+
+        // トークンがある場合、プッシュ通知を行う
+        if (token !== "" && token !== undefined) {
+          await axios.post("https://exp.host/--/api/v2/push/send", {
+            to: token,
+            title: `フォロー！`,
+            body: `${currentUser.username}さんがフォローしました。`,
+            data: {
+              type: "follow",
+              id: currentUser.id,
+            },
+          });
+        }
+
         return true;
       } catch (e) {
         console.log(e);
